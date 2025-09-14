b81f2595cf9c148a660c0b690b4220654e49c435
MJ-Jiang <maura_jiang@outlook.com>
2025-09-14 13:47:17 +0300

    debug users_api.test

--- bloglist-backend/test/users_api.test.js
+++ bloglist-backend/test/users_api.test.js
@@ -5,34 +5,35 @@ const mongoose = require('mongoose')
 const supertest = require('supertest')
 const helper = require('./test_helper')
 const User = require('../models/user')
+const blog = require('../models/blog')
 
 const api = supertest(app)
 
 describe('users', () => {
   beforeEach(async () => {
-    await User.deleteMany({})
+    await User.deleteMany({ username: /^user_test_/ })
   })
 
   test('a valid user can be added', async () => {
     const newUser = {
-      username: 'newuser',
+      username: 'user_test_newuser',
       name: 'New User',
       password: 'test123'
     }
 
-    const usersAtStart = await helper.usersInDb()
+    const usersAtStart = (await helper.usersInDb()).filter(u => u.username.startsWith('user_test_'))
 
     const response = await api
       .post('/api/users')
       .send(newUser)
       .expect(201)
       .expect('Content-Type', /application\/json/)
-
+    console.log('create user response:', response.body)
     assert.strictEqual(response.body.username, newUser.username)
     assert.strictEqual(response.body.name, newUser.name)
 
-    const usersAtEnd = await helper.usersInDb()
-
+    const usersAtEnd = (await helper.usersInDb()).filter(u => u.username.startsWith('user_test_'))
+    console.log('usersAtEnd:', usersAtEnd)
     assert.strictEqual(usersAtStart.length, usersAtEnd.length - 1)
   })
 
